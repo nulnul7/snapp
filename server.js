a216c3228f56be0e8f49142f30fe845d6abc5015
Bell Oluwatomisin <bello4aus@gmail.com>
2021-11-02 23:19:32 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const expressSession = require('express-session');
+
+const Auth0Strategy = require("passport-auth0");
+const authRouter = require ()
+const port = process.env.PORT || 5000;
+
+//Start a server instance with express 
+const app = express();
+const router = express.Router();
